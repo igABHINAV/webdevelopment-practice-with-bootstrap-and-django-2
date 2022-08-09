9a41e15c3e1db99c26408bbb197a7883e7c948a7
Abhinav <abhinav2003feb@gmail.com>
2022-08-09 17:29:32 +0530

    initial commit in nwefeature branch

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,16 @@
+import imp
 from django.shortcuts import render
-
+from .models import Product
+import math
 # Create your views here.
 def index(req):
-    return render(req,'home.html')
+    p=Product.objects.all()
+    n=len(p)
+    slides=n//4+math.ceil((n/4)-(n//4))
+    c=Product.objects.values('product_category')
+    print(c)
+    params={'p':p,'range':range(n),'slides':slides}
+    return render(req,'home.html',params)
 def AddtoList(req):
+    
     return render(req,'insert.html')    
