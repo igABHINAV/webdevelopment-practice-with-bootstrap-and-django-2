f2e8f70833c8d2a03a1da9043a66a237f9507e49
Abhinav <abhinav2003feb@gmail.com>
2022-08-09 21:05:54 +0530

    category wise carousels in newfeature branch and now planning to merge into main branch

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,26 @@
 import imp
+from itertools import product
 from django.shortcuts import render
 from .models import Product
 import math
 # Create your views here.
 def index(req):
-    p=Product.objects.all()
-    n=len(p)
-    slides=n//4+math.ceil((n/4)-(n//4))
+    # p=Product.objects.all()
+    # n=len(p)
+    
+    allprods=[]
     c=Product.objects.values('product_category')
+    cat={item['product_category'] for item in c}
     print(c)
-    params=[[p,range(n),slides],[p,range(n),slides]]
-    ap={'ap':params}
+    for i in cat :
+        prod=Product.objects.filter(product_category=i)
+        n=len(prod)
+        slides=n//4+math.ceil((n/4)-(n//4))
+        allprods.append([prod,range(1,slides),slides])
+
+
+    # params=[[p,range(n),slides],[p,range(n),slides]]
+    ap={'ap':allprods}
     return render(req,'home.html',ap)
 def AddtoList(req):
     
