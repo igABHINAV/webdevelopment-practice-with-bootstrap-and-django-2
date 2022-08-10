0e4bf08a6950f3538a583bfbb4fd8a922a6ada19
Abhinav <abhinav2003feb@gmail.com>
2022-08-10 18:20:03 +0530

    login feature is added successfully

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,33 @@
+from http.client import HTTPResponse
 import imp
 from itertools import product
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.auth import logout , login , authenticate
 import math
-# Create your views here.
+# Create your views here.   1234!@#$qwer
 def index(req):
     # p=Product.objects.all()
     # n=len(p)
+    # if req.user.is_anonymous :
+    #     return redirect(req,'insert.html')
+    if req.method=="POST":
+
+        Username=req.POST.get('emaill')
+        password=req.POST.get('passwordy')
+        useri=authenticate(req,username=Username,password=password)
+        if useri is not None:
+            login(req ,useri)
+            params={'name':Username}
+            return render(req,'insert.html',params) 
+
+        
+    else :
+        return render(req,'home.html')        
+    
+    
+    
     
     allprods=[]
     c=Product.objects.values('product_category')
